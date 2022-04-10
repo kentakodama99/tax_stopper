764917ae4dd7ba47556dd1e9f32d9c6a076f178f
KentaKodama99 <kenta.kodama22jyt22@gmail.com>
2022-04-10 20:54:57 +0900

    Apacheに反映スクリプト

---
+++ src/build.sh
@@ -0,0 +1,29 @@
+#!/bin/bash
+#
+#sudo chmod 755 build.sh
+#sudo bash build.sh
+#
+
+APP="app"
+
+
+function checkDir () {
+    if [ ! -e $1 ]; then
+        echo "no such directory: $1"
+        echo -e "exit\n"
+        exit 0
+    fi
+}
+
+CURRENT=$(cd $(dirname $0)/$APP;pwd)
+checkDir $CURRENT
+cd $CURRENT
+
+echo -e "\nRun build...\n"
+npm run build
+
+checkDir dist
+cd dist
+
+cp -a * /usr/local/apache2/htdocs/
+echo "Build completed"
